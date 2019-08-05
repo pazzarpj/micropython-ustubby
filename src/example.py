a808d4b4385b2c6417f66ccc3982235f8c2c9839
ryanpj <ryan.parry-jones@ampcontrolgroup.com>
2019-08-05 16:06:12 +1000

    Added module level stubbing

---
+++ src/example.py
@@ -0,0 +1,15 @@
+def add_ints(a: int, b: int) -> int:
+    """Adds two integers
+    :param a:
+    :param b:
+    :return:a + b"""
+
+
+def concat_strings(str1: str, str2: str, str3: str) -> str:
+    """
+    Concat two strings
+    :param str1:
+    :param str2:
+    :param str3:
+    :return:
+    """
