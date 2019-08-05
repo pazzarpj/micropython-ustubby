ac1a29982d2ac27199be0406ac270b0ffb2ff9a0
pazzarpj <6457830+pazzarpj@users.noreply.github.com>
2019-08-05 16:56:21 +1000

    Adding support for object and a new test case output string to check against

--- src/example.py
+++ src/example.py
@@ -5,7 +5,7 @@ def add_ints(a: int, b: int) -> int:
     :return:a + b"""
 
 
-def concat_strings(str1: str, str2: str, str3: str) -> str:
+def concat_strings(str1: str, str2: str, str3: str, str4: str, str5: str) -> str:
     """
     Concat two strings
     :param str1:
@@ -13,3 +13,21 @@ def concat_strings(str1: str, str2: str, str3: str) -> str:
     :param str3:
     :return:
     """
+
+
+#
+# def foo(a, b, c, d):
+#     pass
+#
+# def foo(*args):
+#     pass
+#
+# def foo(*args, **kwargs):
+#     pass
+#
+#
+# def foo(a, b, c, d=None, **kwargs):
+#     pass
+
+def readfrommem(addr: int, memaddr: int, arg: object, *, addrsize: int = 8) -> str:
+    pass
