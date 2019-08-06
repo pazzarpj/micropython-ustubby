f33484319004dc68b4f2b3a4d15752dd7cf4f326
pazzarpj <6457830+pazzarpj@users.noreply.github.com>
2019-08-06 11:38:26 +1000

    Passes test with complex kwargs example

--- src/example.py
+++ src/example.py
@@ -5,14 +5,14 @@ def add_ints(a: int, b: int) -> int:
     :return:a + b"""
 
 
-def concat_strings(str1: str, str2: str, str3: str, str4: str, str5: str) -> str:
-    """
-    Concat two strings
-    :param str1:
-    :param str2:
-    :param str3:
-    :return:
-    """
+# def concat_strings(str1: str, str2: str, str3: str, str4: str, *, str5: str, ) -> str:
+#     """
+#     Concat two strings
+#     :param str1:
+#     :param str2:
+#     :param str3:
+#     :return:
+#     """
 
 
 #
@@ -29,5 +29,11 @@ def concat_strings(str1: str, str2: str, str3: str, str4: str, str5: str) -> str
 # def foo(a, b, c, d=None, **kwargs):
 #     pass
 
-def readfrommem(addr: int, memaddr: int, arg: object, *, addrsize: int = 8) -> str:
-    pass
+def readfrom_mem(addr: int = 0, memaddr: int = 0, arg: object = None, *, addrsize: int = 8) -> str:
+    """
+    :param addr:
+    :param memaddr:
+    :param arg:
+    :param addrsize:
+    :return:
+    """
